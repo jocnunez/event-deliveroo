e5b131f306c956b66c0276e2f1fe551062af4008
Jose Núñez <hose@MacBook-Pro-de-Jose.local>
2019-10-30 00:08:11 +0100

    create docker image for web

--- environment/build-images.sh
+++ environment/build-images.sh
@@ -5,3 +5,4 @@ CURRENT_DIR=$PWD
 cd ../order      && mvn clean install && docker build -t orderapp . && cd $CURRENT_DIR
 cd ../kitchen    && mvn clean install && docker build -t kitchenapp . && cd $CURRENT_DIR
 cd ../delivery   && mvn clean install && docker build -t deliveryapp . && cd $CURRENT_DIR
+cd ../web        && npm i && npm run build && docker build -t webapp . && cd $CURRENT_DIR
